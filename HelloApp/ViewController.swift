030edbcfa9ab55945e12ca1de566120fa11f2251
kikyumi <93177162+kikyumi@users.noreply.github.com>
2021-11-05 23:05:32 +0900

    Initial Commit

---
+++ HelloApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloApp
+//
+//  Created by 菊川 由美 on 2021/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
